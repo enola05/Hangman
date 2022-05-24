e3d7c38c53c7375d743d0c4b58107d794fcfab59
Enola Lesni Coelho <69950177+enola05@users.noreply.github.com>
2022-05-25 00:03:27 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+import hangman_art
+import hangman_words
+from replit import clear
+
+print(hangman_art.logo)
+lives=6
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(hangman_words.word_list)
+#print(chosen_word)
+
+answer = ['_']*len(chosen_word)
+print(answer)
+flag=0
+while flag==0 :
+  if '_' in answer:
+    guess= input("make a guess : ").lower()
+    clear()
+    if guess not in chosen_word:
+      lives=lives-1
+    for i in range(0,len(chosen_word)):
+      if chosen_word[i] == guess:
+        if answer[i]=='_':
+          answer[i]= guess 
+    print(f"{' '.join(answer)}")
+    #print(answer)
+    print(hangman_art.stages[lives])
+    if lives==0:
+      print("you loose")
+      flag=1
+  else:
+    print("you win")
+    flag=1
+    
